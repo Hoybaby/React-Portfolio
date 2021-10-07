81be48164fe7579f695cff0befd746ed5d9ff1e8
Hoybaby <mbartek436@gmail.com>
2021-10-07 10:07:29 -0400

    gettting all the proper imports and just initial set up to work

---
+++ src/components/body/body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div>
+            this is a body component
+        </div>
+    )
+}
+
+export default Body;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 import Header from '../Header/Header'
+import Body from '../Body/Body';
+import Footer from '../Footer/Footer';
 
 import './home.css'
 
 function Home() {
     return (
         <div className="home">
-            <div>
                 <Header/>
-            </div>
+                <Body/>
+                <Footer/>
+
         </div>
     )
 }
