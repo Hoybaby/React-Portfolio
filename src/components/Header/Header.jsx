969edb82a87b6d6cc854cd0811d3c1273dc3ec71
Hoybaby <mbartek436@gmail.com>
2021-10-07 10:47:19 -0400

    making more files for the header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import './Header.css'
+
 function Header() {
     return (
-        <div>
-            this is a header
+        <div className="header">
+            <div className="logo">
+                Michael Bartek
+            </div>
+            <div className="menu">
+                <div className="web-menu">
+                    Web
+                </div>
+                <div className="mobile-menu">
+                    Mobile
+                </div>
+            </div>
+                
         </div>
     )
 }
