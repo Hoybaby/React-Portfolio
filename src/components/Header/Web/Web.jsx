73227e1ec7fe83c56e4512b5ade852c3196751e2
Hoybaby <mbartek436@gmail.com>
2021-10-07 17:51:27 -0400

    added some icons for my menu bar on the web menu

--- src/components/Header/Web/Web.jsx
+++ src/components/Header/Web/Web.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Web.css'
+
+import {FcEditImage} from 'react-icons/fc';
+import {AiOutlineLaptop, AiOutlineUser} from 'react-icons/ai';
+import {BsFillBriefcaseFill} from 'react-icons/bs';
+
+
 
 function Web() {
     return (
-        <div>
-            this is web
+        <div className="web">
+            <div className="web-option">
+                <a href="#project"> <FcEditImage/> Projects </a>
+            </div>
+            <div className="web-option">
+                <a href="#skills"><AiOutlineLaptop/>Skills</a>
+            </div>
+            <div className="web-option">
+                <a href="#About"><BsFillBriefcaseFill/>Work</a>
+            </div>
+            <div className="web-option">
+                <a href="#filler"><AiOutlineUser/>Contact</a>
+            </div>
         </div>
     )
 }
