968b7f375a05572360dfa5307e122d0b7e22ea25
Hoybaby <mbartek436@gmail.com>
2021-10-12 15:30:43 -0400

    adding all the components for the body and i am adding information to the about seciotn.

--- src/components/body/About/About.jsx
+++ src/components/body/About/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './About.css'
+
 
 function About() {
     return (
-        <div>
-            This is the about section
+        <div className='about'>
+            <div className="about-top">
+                <div className="about-info">
+                    Hello! My name is <br/> Michael Bartek  <br/>
+                    and I love creating applications with JavaScript!
+                </div>
+                <div className="about-photo">
+                    <img 
+                    src={require('../../../assets/coding.png').default} 
+                    className='picture'/>
+                </div>
+            </div>
+            <div className="about-bottom">
+                this is contact buttons
+            </div>
+            
         </div>
     )
 }
--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './Body.css'
 
+import About from './About/About';
+import Projects from './Projects/Projects'
+import Skills from './Skills/Skills';
+import Work from './Work/Work';
+import Contact from './Contact/Contact';
+
+
 function Body() {
     return (
         <div className="body">
-            this is a body component
+            <section id='about'>
+                <About/>
+            </section>
+            <section id="projects">
+                <Projects/>
+            </section>
+            <section id="skills">
+                <Skills />
+            </section>
+            <section id='work'>
+                <Work />
+            </section>
+            <section id='contact'>
+                <Contact />
+            </section>
+
         </div>
     )
 }
