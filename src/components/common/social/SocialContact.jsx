2601a40f05ec94eacaf015b743b8c22119558886
Hoybaby <mbartek436@gmail.com>
2021-10-12 23:22:02 -0400

    trying to make the social icons work with an array for a mapiong sequence on the social aspect that I will ahve twice on my portfolio

--- src/components/common/social/SocialContact.jsx
+++ src/components/common/social/SocialContact.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { SocialData } from '../../data/social';
 import './SocialContact.css'
 
 function SocialContact() {
+
+    const data = SocialData;
     return (
-        <div>
-            
+        <div className='social-contact'>
+            {data.map((item) => {
+                return( 
+                <a href={item.link}>
+                    <div className='social-icon-div'>
+
+                    </div>
+                </a>
+                )
+            })}
         </div>
     )
 }
 
-export default SocialContact
+export default SocialContact;
---
+++ src/components/data/social.js
@@ -0,0 +1,17 @@
+export const SocialData = [
+    {
+        plateform: "GitHub",
+        link: "https://github.com/Hoybaby",
+        icon: require('../../assets/github.png').default
+    },
+    {
+        plateform: "LinkedIn",
+        link: "https://www.linkedin.com/in/michaelbartek/",
+        icon: require('../../assets/linkedin.png').default
+    },
+    {
+        plateform: "Instagram",
+        link: "https://www.instagram.com/mbartek436/",
+        icon: require('../../assets/instagram.png').default
+    },
+];
