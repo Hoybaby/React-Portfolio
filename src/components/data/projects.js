72de266e655b16a3bc36bae40379b103086e1879
Hoybaby <mbartek436@gmail.com>
2021-10-15 15:44:25 -0400

    added styling to my project to make sure that the projects card are working like jhow it should. Ran into an issue with styling my tags and had to go back to my project folder/file toname the array:

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -4,7 +4,7 @@ export const ProjectData = [
         id: 1,
         title: "React-MovieDB",
         about: 'In this project, a user is able to look up different movies with the IMDB Movie API and see basic information of the movie.',
-        tags: ["ReactJs, NodeJS, StyledComponents, JavaScript, CSS"],
+        tags: ["ReactJs", "NodeJS", "StyledComponents", "JavaScript", "CSS"],
         demo: 'https://heuristic-kepler-a0b406.netlify.app/',
         github: 'https://github.com/Hoybaby/React-MovieDB',
         image: require('../../assets/reactMovieDb.PNG').default
@@ -13,7 +13,7 @@ export const ProjectData = [
         id: 2,
         title: "React-Social-App",
         about: 'Individuals can come together and share posts about anything they want. A individual must first register and log in to be able to access these features which uses JWToken for authentication',
-        tags: ["ReactJs, NodeJS, MONGODB, GraphQL, JavaScript,"],
+        tags: ["ReactJs"," NodeJS", "MongoDB", "GraphQL", "JavaScript"],
         demo: 'https://elated-engelbart-fcaedd.netlify.app/',
         github: 'https://github.com/Hoybaby/React-Social-App',
         image: require('../../assets/react-social-app.PNG').default
