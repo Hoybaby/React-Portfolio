80930677614ed96814cb5d1792eac3d52010a1df
Hoybaby <mbartek436@gmail.com>
2021-10-28 20:19:45 -0400

    testing one last time

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Header from '../Header/Header'
+import Header from '../Header/Header';
 import Body from '../Body/Body';
 import Footer from '../Footer/Footer';
 
@@ -12,7 +12,6 @@ function Home() {
                 <Header/>
                 <Body/>
                 <Footer/>
-
         </div>
     )
 }
